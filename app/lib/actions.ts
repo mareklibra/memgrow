0cde2f1d43db6fbac91a5ef6c5a998daa4939473
Marek Libra <marek.libra@gmail.com>
2024-11-17 15:38:25 +0100

    Updating words works

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,6 @@
 "use server";
 
 import { sql } from "@vercel/postgres";
-// import { revalidatePath } from "next/cache";
-// import { redirect } from "next/navigation";
 
 import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
@@ -18,106 +16,25 @@ export async function updateWordProgress(word: Word) {
   } catch {
     return { message: "Database Error: Failed to update word progress." };
   }
-  // revalidatePath("/dashboard/invoices");
-  // redirect("/dashboard/invoices");
 }
 
-/*
-export type State = {
-  errors?: {
-    customerId?: string[];
-    amount?: string[];
-    status?: string[];
-  };
-  message?: string | null;
-};
-
-
-const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string({
-    invalid_type_error: "Please select a customer.",
-  }),
-  amount: z.coerce
-    .number()
-    .gt(0, { message: "Please enter an amount greater than $0." }),
-  status: z.enum(["pending", "paid"], {
-    invalid_type_error: "Please select an invoice status.",
-  }),
-  date: z.string(),
-});
-
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
-export async function createInvoice(prevState: State, formData: FormData) {
-  const rawFormData = {
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  };
-  // Object.fromEntries(formData.entries())
-  const validatedFields = CreateInvoice.safeParse(rawFormData);
-  // If form validation fails, return errors early. Otherwise, continue.
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: "Missing Fields. Failed to Create Invoice.",
-    };
-  }
-
-  // Prepare data for insertion into the database
-  const { customerId, amount, status } = validatedFields.data;
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split("T")[0];
-
+export async function updateWord(changed: Word) {
   try {
     await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
-  } catch {
-    return {
-      message: "Database Error: Failed to Create Invoice.",
-    };
-  }
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices"); // this throws error, so keep it outside try {}
-}
-
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  });
-
-  const amountInCents = amount * 100;
-
-  try {
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
+      UPDATE words
+      SET 
+        word = ${changed.word},
+        definition = ${changed.definition},
+        memlevel = ${changed.memLevel}, 
+        form = ${changed.form}
+      WHERE id = ${changed.id}
     `;
-  } catch {
-    return { message: "Database Error: Failed to Update Invoice." };
-  }
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
-}
-
-export async function deleteInvoice(id: string) {
-  // throw new Error("Failed to Delete Invoice");
-
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice." };
-  } catch {
-    return { message: "Database Error: Failed to Delete Invoice." };
+  } catch (e) {
+    return {
+      message: `Database Error: Failed to update word. ${JSON.stringify(e)}`,
+    };
   }
 }
-*/
 
 export async function authenticate(
   prevState: string | undefined,
--- app/ui/EditWords.tsx
+++ app/ui/EditWords.tsx
@@ -1,33 +1,120 @@
 "use client";
 
+import { MouseEvent, useCallback, useState } from "react";
+import isEqual from "lodash/isEqual";
+
 import { Word } from "@/app/lib/definitions";
+import { updateWord } from "@/app/lib/actions";
+import { Button } from "./button";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 interface EditWordsProps {
   words: Word[];
 }
 
+const thClass =
+  "px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500";
+const tdClass =
+  "px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200";
+const inputClass =
+  "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+
+function WordRow({ word }: { word: Word }) {
+  const [old, setOld] = useState<Word>(word);
+  const [changed, setChanged] = useState<Word>(word);
+  const [error, setError] = useState<string>();
+
+  const handleSave = useCallback(
+    async (e: MouseEvent) => {
+      e.preventDefault();
+      const result = await updateWord(changed);
+      setError(result?.message);
+      if (result) {
+        console.error(result);
+      } else {
+        setOld(changed);
+      }
+    },
+    [changed]
+  );
+
+  return (
+    <tr id={word.id}>
+      <td className={tdClass}>
+        <input
+          type="text"
+          className={inputClass}
+          required
+          value={changed.word}
+          onChange={(e) => {
+            setChanged({ ...changed, word: e.currentTarget.value });
+          }}
+        />
+      </td>
+      <td className={tdClass}>
+        <input
+          type="text"
+          className={inputClass}
+          required
+          value={changed.definition}
+          onChange={(e) => {
+            setChanged({ ...changed, definition: e.currentTarget.value });
+          }}
+        />
+      </td>
+      <td className={tdClass}>
+        <input
+          type="number"
+          className={inputClass}
+          required
+          value={changed.memLevel}
+          onChange={(e) => {
+            setChanged({ ...changed, memLevel: Number(e.currentTarget.value) });
+          }}
+        />
+      </td>
+      <td className={tdClass}>{changed.form}</td>
+      <td className={tdClass}>
+        <div className="flex flex-row">
+          <Button disabled={isEqual(old, changed)} onClick={handleSave}>
+            Save
+          </Button>
+          {error && <ExclamationTriangleIcon className="text-red-500 w-8" />}
+        </div>
+      </td>
+    </tr>
+  );
+}
+
 export function EditWords({ words }: EditWordsProps) {
   return (
-    <table className="border-separate border border-slate-500">
-      <thead>
-        <tr>
-          <th className="border border-slate-600">Word</th>
-          <th className="border border-slate-600">Definition</th>
-          <th className="border border-slate-600">Memory Level</th>
-          <th className="border border-slate-600">  Next Form</th>
-        </tr>
-      </thead>
-
-      {words.map((w) => {
-        return (
-          <tr key={w.id} id={w.id}>
-            <td>{w.word}</td>
-            <td>{w.definition}</td>
-            <td>{w.memLevel}</td>
-            <td>{w.form}</td>
+    <form>
+      <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+        <thead>
+          <tr>
+            <th scope="col" className={thClass}>
+              Word
+            </th>
+            <th scope="col" className={thClass}>
+              Definition
+            </th>
+            <th scope="col" className={thClass}>
+              Memory Level
+            </th>
+            <th scope="col" className={thClass}>
+              Next Form
+            </th>
+            <th scope="col" className={thClass}>
+              Action
+            </th>
           </tr>
-        );
-      })}
-    </table>
+        </thead>
+        <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
+          {words.map((w) => (
+            <WordRow word={w} key={w.id} />
+          ))}
+        </tbody>
+      </table>
+    </form>
   );
 }
